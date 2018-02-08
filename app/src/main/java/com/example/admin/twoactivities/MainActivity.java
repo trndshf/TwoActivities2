6f84520c5e921207d3c02534658370e989d5649e
trndshf <triandi.shafa@gmail.com>
2018-02-08 19:25:24 +0700

    Initial commit

---
+++ app/src/main/java/com/example/admin/twoactivities/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.admin.twoactivities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    public static final String EXTRA_MESSAGE = "com.example.android.twoactivities.extra.MESSAGE";
+    public static final int TEXT_REQUEST = 1;
+
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
+    private EditText mMessageEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mMessageEditText = (EditText) findViewById(R.id.editText_main);
+        mReplyHeadTextView = (TextView) findViewById(R.id.text_header_reply);
+        mReplyTextView = (TextView) findViewById(R.id.text_message_reply);
+
+        Log.d(LOG_TAG, "-------");
+        Log.d(LOG_TAG, "onCreate");
+
+        // Initialize all the view variables.
+        mMessageEditText = (EditText) findViewById(R.id.editText_main);
+        mReplyHeadTextView = (TextView) findViewById(R.id.text_header_reply);
+        mReplyTextView = (TextView) findViewById(R.id.text_message_reply);
+
+        // Restore the saved state. See onSaveInstanceState() for what gets saved.
+        if	(savedInstanceState	!=	null)	{
+            boolean	isVisible	=	savedInstanceState.getBoolean("reply_visible");
+            //	Show	both	the	header	and	the	message	views.	If	isVisible	is
+            //	false	or	missing	from	the	bundle,	use	the	default	layout.
+            if	(isVisible)	{
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(savedInstanceState.getString("reply_text"));
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    public void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+        Context context=getApplicationContext();
+        Toast toast = Toast.makeText(context, R.string.toast_messagge2, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+        Context context=getApplicationContext();
+        Toast toast = Toast.makeText(context, R.string.toast_messagge, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, SecondActivity.class);
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+    public void onActivityResult(int requestCode, int resultCode,
+                                Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String reply =
+                        data.getStringExtra(SecondActivity.EXTRA_REPLY);
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(reply);
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+// If the heading is visible, we have a message that needs to be saved.
+// Otherwise we're still using default layout.
+        if (mReplyHeadTextView.getVisibility() == View.VISIBLE) {
+            outState.putBoolean("reply_visible", true);
+            outState.putString("reply_text", mReplyTextView.getText().toString());
+        }
+    }
+}
